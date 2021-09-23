79d7b62cfe6e9779ed27546ebda2cdcafee0a7a0
Alexis S Liu <syl1350@gmail.com>
2021-09-23 12:00:02 -0400

    commit tree c52044582e17e1904f1b28b9007ab038b31e44a5

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,47 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-  def index
+
+def index
+  if params[:user_id]
+    user = User.find(params[:user_id])
+    items = user.items
+  else
     items = Item.all
-    render json: items, include: :user
+  end
+  render json: items, include: :user
+end
+
+ def show
+    item = find_item
+    render json: item
+  end
+
+def create
+  user = find_user
+  item = user.items.create(item_params)
+  render json: item, status: :created
+end
+
+ private
+
+ def item_params
+    params.permit(:name, :description, :price)
+  end
+
+def find_user
+
+  User.find(params[:user_id])
+end
+
+def find_item
+
+  Item.find(params[:id])
+end
+
+
+def render_not_found_response(exception)
+    render json: { error: "#{exception.model} not found" }, status: :not_found
   end
 
 end
